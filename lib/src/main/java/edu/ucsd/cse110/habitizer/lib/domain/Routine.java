269b390d5ea19e687c077753dbec399dfb09b4bf
Kameron Gano <kgano@ucsd.edu>
2025-02-05 13:46:44 -0800

    Added id for routine, task

--- lib/src/main/java/edu/ucsd/cse110/habitizer/lib/domain/Routine.java
+++ lib/src/main/java/edu/ucsd/cse110/habitizer/lib/domain/Routine.java
@@ -4,11 +4,16 @@ import java.util.ArrayList;
 
 public class Routine {
     private ArrayList<Task> tasks;
+    private final Integer id;
 
-    public Routine(ArrayList<Task> tasks) {
+    public Routine(ArrayList<Task> tasks, Integer id) {
         this.tasks = tasks;
+        this.id = id;
     }
 
+    public Integer id() {
+        return this.id;
+    }
     public ArrayList<Task> getTasks() {
         return this.tasks;
     }
--- lib/src/main/java/edu/ucsd/cse110/habitizer/lib/domain/Task.java
+++ lib/src/main/java/edu/ucsd/cse110/habitizer/lib/domain/Task.java
@@ -1,19 +1,41 @@
 package edu.ucsd.cse110.habitizer.lib.domain;
 
+import androidx.annotation.Nullable;
+
+import java.util.Objects;
+
 public class Task {
 
     private String name;
+    private final Integer id;
     // some sort of time field, not ready for this yet
     // ... more fields to come
-    public Task(String name) {
+    public Task(String name, Integer id) {
         this.name = name;
+        this.id = id;
     }
 
+    public @Nullable Integer id() {
+        return this.id;
+    }
     public String getName() {
         return this.name;
     }
 
     public void setName(String newName) {
         this.name = newName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(name, task.name) && Objects.equals(id, task.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id);
     }
 }
