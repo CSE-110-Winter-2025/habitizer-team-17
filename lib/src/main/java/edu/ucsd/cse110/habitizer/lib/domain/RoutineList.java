9de8809c421efac4aa2b7cb251de7bcb8fad76de
Kameron Gano <kgano@ucsd.edu>
2025-02-05 14:15:53 -0800

    id removed from routine list

--- lib/src/main/java/edu/ucsd/cse110/habitizer/lib/domain/RoutineList.java
+++ lib/src/main/java/edu/ucsd/cse110/habitizer/lib/domain/RoutineList.java
@@ -5,18 +5,13 @@ import java.util.List;
 
 public class RoutineList {
     List<Routine> routines;
-    private Integer id;
 
-    public RoutineList(List<Routine> routines, Integer id) {
+    public RoutineList(List<Routine> routines) {
         this.routines = routines;
-        this.id = id;
     }
 
     public List<Routine> getRoutines() {
         return this.routines;
     }
 
-    public Integer id() {
-        return  this.id;
-    }
 }
