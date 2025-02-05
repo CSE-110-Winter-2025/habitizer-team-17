d033a546fa231f303fa9633487d6e764a93dae6f
Kameron Gano <kgano@ucsd.edu>
2025-02-05 14:11:27 -0800

    Id added to routine list

--- lib/src/main/java/edu/ucsd/cse110/habitizer/lib/domain/RoutineList.java
+++ lib/src/main/java/edu/ucsd/cse110/habitizer/lib/domain/RoutineList.java
@@ -5,12 +5,18 @@ import java.util.List;
 
 public class RoutineList {
     List<Routine> routines;
+    private Integer id;
 
-    public RoutineList(List<Routine> routines) {
+    public RoutineList(List<Routine> routines, Integer id) {
         this.routines = routines;
+        this.id = id;
     }
 
     public List<Routine> getRoutines() {
         return this.routines;
+    }
+
+    public Integer id() {
+        return  this.id;
     }
 }
