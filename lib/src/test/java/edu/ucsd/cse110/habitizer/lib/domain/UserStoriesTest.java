8fd1c89761aab34e54a5a2666ded75e5ae64ca77
palak-1402 <128050678+palak-1402@users.noreply.github.com>
2025-02-16 14:13:09 -0800

    added tests

--- lib/src/test/java/edu/ucsd/cse110/habitizer/lib/domain/UserStoriesTest.java
+++ lib/src/test/java/edu/ucsd/cse110/habitizer/lib/domain/UserStoriesTest.java
@@ -3,79 +3,80 @@ package edu.ucsd.cse110.habitizer.lib.domain;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import edu.ucsd.cse110.habitizer.lib.data.InMemoryDataSource;
 
+/**
+ * Tests for US6 (tracking checked-off task time) and US10 (renaming tasks).
+ */
 public class UserStoriesTest {
 
     @Test
     public void testCheckedOffTaskStoresElapsedTime() {
         var task = new Task(10, "Example Task");
-        var routine = new Routine(1, "Morning Routine", List.of(task), 30);
+        var routine = new Routine(1, "Morning Routine", List.of(task), 30, 0);
         var activeTask = new ActiveTask(task, false, 0);
         var activeRoutine = new ActiveRoutine(routine, List.of(activeTask));
-
         long fiveMinutesMs = 5 * 60 * 1000;
 
         var updatedTask = activeTask.withChecked(true, fiveMinutesMs);
         var updatedActiveRoutine = activeRoutine.withActiveTask(updatedTask);
+
         assertTrue(updatedActiveRoutine.activeTasks().get(0).checked());
         assertEquals(fiveMinutesMs,
                 updatedActiveRoutine.activeTasks().get(0).checkedElapsedTime());
     }
-
     @Test
     public void testDisplayMinutesForCheckedOffTask() {
         long fiveMinutesMs = 5 * 60 * 1000;
-
-        long minutes = fiveMinutesMs / 60000;
+        long minutes = fiveMinutesMs / 60_000;
         String display = minutes + "m";
 
         assertEquals(5, minutes);
         assertEquals("5m", display);
     }
 
-    @Test
-    public void testRenameTaskInRepository() {
-        var dataSource = new InMemoryDataSource();
-        var taskRepo = new TaskRepository(dataSource);
-
-        var originalTask = new Task(5, "Original Name");
-        taskRepo.save(originalTask);
-        taskRepo.rename(5, "New Name");
-        Task updatedTask = taskRepo.find(5).getValue();
-        assertNotNull(updatedTask);
-        assertEquals("New Name", updatedTask.name());
-    }
 
     @Test
     public void testRenameTaskInRoutine() {
         var dataSource = new InMemoryDataSource();
-        var taskRepo = new TaskRepository(dataSource);
         var routineRepo = new RoutineRepository(dataSource);
 
         var task = new Task(101, "Old Name");
-        taskRepo.save(task);
-
-        var routine = new Routine(201, "My Routine", List.of(task), 20);
+        var routine = new Routine(201, "My Routine", List.of(task), 20, 0);
         routineRepo.save(routine);
 
-        taskRepo.rename(task.id(), "Brand New Name");
+        var updatedRoutine = routine.withRenamedTask(101, "Brand New Name");
+        routineRepo.save(updatedRoutine);
 
-        Routine updatedRoutine = routineRepo.find(201).getValue();
-        assertNotNull(updatedRoutine);
+        Routine fetched = routineRepo.find(201).getValue();
+        assertNotNull(fetched);
 
-        var refreshedTasks = new ArrayList<Task>();
-        for (Task t : updatedRoutine.tasks()) {
-            var latest = taskRepo.find(t.id()).getValue();
-            refreshedTasks.add(latest);
-        }
-        var finalRoutine = updatedRoutine.withTasks(refreshedTasks);
-
-        Task updatedTask = finalRoutine.tasks().get(0);
-        assertEquals("Brand New Name", updatedTask.name());
+        var renamedTask = fetched.tasks().get(0);
+        assertEquals("Brand New Name", renamedTask.name());
     }
 
+    @Test
+    public void testRenameOneTaskAmongMany() {
+        var dataSource = new InMemoryDataSource();
+        var routineRepo = new RoutineRepository(dataSource);
+
+        var task1 = new Task(1, "First Task");
+        var task2 = new Task(2, "Second Task");
+        var task3 = new Task(3, "Third Task");
+        var routine = new Routine(999, "Multi-task Routine",
+                List.of(task1, task2, task3), 30, 0);
+        routineRepo.save(routine);
+
+        var updatedRoutine = routine.withRenamedTask(2, "Renamed Second Task");
+        routineRepo.save(updatedRoutine);
+
+        Routine fetched = routineRepo.find(999).getValue();
+        assertNotNull(fetched);
+        var tasks = fetched.tasks();
+        assertEquals("First Task", tasks.get(0).name());
+        assertEquals("Renamed Second Task", tasks.get(1).name());
+        assertEquals("Third Task", tasks.get(2).name());
+    }
 }
